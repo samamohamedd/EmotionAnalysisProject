573eb706c77e7e6b7d2e3c8796ed1873d5b2712d
Sama mohamed mahmoud abd ellatif <143009290+samamohamedd@users.noreply.github.com>
2025-01-11 19:24:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # Load the pre-trained model
 model = pickle.load(
-        open("/mount/src/emotionanalysisproject/app/models/model.pkl", "rb")
+        open("/mount/src/emotionanalysisproject/app/model.pkl", "rb")
     )
 
 # Define emotion labels
